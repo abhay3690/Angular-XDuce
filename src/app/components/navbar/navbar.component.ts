d09e384f467d3485dd7b6f9a2fc6f78be869f54b
abhay3690 <abhay7777@gmail.com>
2024-06-20 16:22:07 +0530

    Add footer

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [FooterComponent],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+
+export class NavbarComponent {
+  
+  
+msg : string = "";
+
+  showinfo(){
+     this.msg ="hello abhay";
+  }
+}
